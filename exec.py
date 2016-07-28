cfb959f2bcecd3dd5f6464ffe8a8b22ce796af79
Kevin Haller <kevin.haller@outofbits.com>
2016-07-28 03:34:43 +0200

    #5: Sets logging level of the default execution to FATAL.

--- exec.py
+++ exec.py
@@ -58,4 +58,5 @@ if __name__ == '__main__':
         logging.basicConfig(level=logging.DEBUG)
     # Path argument
     if args.path:
+        logging.basicConfig(level=logging.FATAL)
         execute_program_file(args.path)
